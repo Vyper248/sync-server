0e8096b2af7710337512ab250313c64a3ba51108
Christopher T Wilson <vyper374@hotmail.com>
2022-04-26 13:40:24 +0100

    Added cors

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const { mergeData, filterDeleted } = require('./utils');
 
+app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
